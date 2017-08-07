a4a79c98d476ae079a3feea95949818e3802523c
MrTaco <castilloherrera@yahoo.com>
2017-08-07 07:58:11 -0600

    Revert "Revert "Upload""
    This reverts commit 74071bac74387d46bef2642bab037c7ddc7b5d3e.

---
+++ calcular areas/funciones.js
@@ -0,0 +1,13 @@
+function Triangulo(base, altura) {
+	return base*altura/2;
+}
+function Cuadrado(base, altura) {
+	return base*altura;
+}
+function Circulo(radio) {
+	return radio*radio*3.14159265359/2;
+}
+function CalcularArea(){
+	return
+	
+}
